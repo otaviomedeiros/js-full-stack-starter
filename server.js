7a4bb4209b745ec7e043b3b10d3e5f4451d6765d
Otavio Medeiros <otaviorm@gmail.com>
2017-06-30 21:50:35 -0300

    Save user on signup

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/jsfullstackstarter');
 
-app.get('/', (req, res) => {
-  res.send('JS full stack starter');
+const { User } = require('./models');
+
+app.use(bodyParser.json());
+
+app.post('/api/signup', (req, res) => {
+  const { email, password } = req.body;
+  const user = new User({ email, password });
+
+  user.save((err, user) => {
+    if (err) {
+      res.send(err);
+    }
+
+    res.json(user);
+  });
 });
 
 app.listen(3000, () => {
