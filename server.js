688c1d76a6cb633b1fcd19b46c6e5aa400cd8699
Otavio Medeiros <otaviorm@gmail.com>
2017-06-30 21:24:10 -0300

    Add mongoose to app

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/jsfullstackstarter');
 
 app.get('/', (req, res) => {
   res.send('JS full stack starter');
