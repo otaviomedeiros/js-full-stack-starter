1a326295bceaf34e131b3565a0fc397875d3bd2d
Otavio Medeiros <otaviorm@gmail.com>
2017-07-04 21:40:23 -0300

    Add passport to make authenticated requests

---
+++ controllers/index_controller.js
@@ -0,0 +1,7 @@
+const index = (req, res) => {
+  res.send('index');
+}
+
+module.exports = {
+  index
+}
--- server.js
+++ server.js
@@ -2,13 +2,44 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const AuthenticationController = require('./controllers/auth_controller');
-require('./config');
+const IndexController = require('./controllers/index_controller');
+require('./db');
+
+const { User } = require('./models');
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const jwtOptions = {
+  secretOrKey: process.env.SECRET,
+  jwtFromRequest: ExtractJwt.fromAuthHeader()
+};
+
+const strategy = new Strategy(jwtOptions, (jwt_payload, next) => {
+  const user = User.findOne({ _id: jwt_payload.uer_id });
+
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+app.use(passport.initialize());
+
 
 app.use(bodyParser.json());
 
+const authenticate = function(){
+  return passport.authenticate('jwt', { session: false });
+}
+
+app.get(
+  '/',
+  authenticate(),
+  IndexController.index
+);
 app.post('/api/signup', AuthenticationController.signup);
 app.post('/api/signin', AuthenticationController.signin);
-
 
 const port = process.env.PORT;
 
