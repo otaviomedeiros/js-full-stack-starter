7f0333ad63734199c9b6fe6b738f66f6334345d3
Otavio Medeiros <otaviorm@gmail.com>
2017-07-13 19:14:21 -0300

    Add webpack and babel to frontend

---
+++ frontend/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'app.bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader' }
+    ]
+  }
+};
