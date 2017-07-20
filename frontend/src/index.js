4b67062e7c08b8c03a8f5276e705f26f76c2a387
Otavio Medeiros <otaviorm@gmail.com>
2017-07-20 20:47:30 -0300

    Add redux and react-redux

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './app';
+import reducers from './reducers';
 
-const App = () => {
-  return <div>App</div>
-}
+const store = createStore(reducers);
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
